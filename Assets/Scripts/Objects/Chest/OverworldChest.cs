452af03dd3426ef4292926dbea8df6ae1bebd1a7
Jarret Horner <JHorn384@mtroyal.ca>
2023-08-02 18:33:11 -0600

    Got chests to save correctly when loading file.

--- Assets/Scripts/Objects/Chest/OverworldChest.cs
+++ Assets/Scripts/Objects/Chest/OverworldChest.cs
@@ -29,7 +29,7 @@ public class OverworldChest : MonoBehaviour
     {
         contextClue = FindObjectOfType<ContextClue>();
         playerUI = FindObjectOfType<PlayerUI>();
-        overworldStateManager = FindObjectOfType<SceneStateManager>().GetOverworldStateManager(0);
+        overworldStateManager = FindObjectOfType<OverworldStateManager>();
         pauseGame = FindObjectOfType<PauseGame>();
 
         Debug.Log("Start chest");
